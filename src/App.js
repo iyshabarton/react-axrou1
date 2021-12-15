ca5ae7adc35137cd1687a693c8320101f2504e30
iyshabarton <iysha-barton_student2021@wilder.school>
2021-12-15 11:52:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Contact = () => {
+  console.log('props:');
+
+  return (
+    <div className="friend-item">
+      <h3>Emmanuel</h3>
+      <h4>myemmanuel@email.com</h4>
+      <h4>123123</h4>
+      <button>Delete</button>
+    </div>
+  );
+};
+
+export const App = (
+  // write your HTML code here
+
+  <div>
+    <h1 className="text-center">My contacts list 📱</h1>
+
+    <div>
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+    </div>
+  </div>
+);
